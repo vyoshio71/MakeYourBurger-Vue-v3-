02197d2afbfe73d89cc2a8cdb684b7c27534a619
Victor Yoshio <yoshio.victor@hotmail.com>
2024-01-12 10:00:35 -0300

    initial config

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <Navbar :logo="logo_src" :alt="app_name" />
+  <router-view />
+  <Footer />
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue"
+
+export default {
+  components: {
+    Navbar,
+    Footer
+  },
+  data() {
+    return {
+      logo_src: "/img/logo.png",
+      app_name: "burguer"
+    }
+  }
+}
+</script>
+
+<style>
+* {
+  font-family: Helvetica;
+  padding: 0;
+  margin: 0;
+  box-sizing: borde-box;
+}
+
+.main-container {
+  margin: 50px;
+  min-height: 60.4vh;
+}
+
+h1 {
+  text-align: center;
+  font-size: 42px;
+  margin-bottom: 30px;
+  color: "#222"
+}
+</style>
